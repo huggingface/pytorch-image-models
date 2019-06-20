1d7f2d93a68bdc3c5d8d634d869709f6cdd7cecd
Ross Wightman <rwightman@gmail.com>
2019-06-19 17:28:08 -0700

    Python >= 3.6 requirement

--- setup.py
+++ setup.py
@@ -33,23 +33,7 @@ setup(
 
     # Note that this is a string of words separated by whitespace, not a list.
     keywords='pytorch pretrained models efficientnet mobilenetv3 mnasnet',
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
     packages=find_packages(exclude=['convert']),
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=['torch', 'torchvision'],
+    python_requires='>=3.6',
 )
