25365c99e13ba4fda81b4368daaabb4e6c4d4ad4
Ross Wightman <rwightman@gmail.com>
2019-08-12 23:06:19 -0700

    Add initial sotabench attempt. Split create_transform out of create_loader. Update requirements.txt

--- timm/data/__init__.py
+++ timm/data/__init__.py
@@ -2,5 +2,5 @@ from .constants import *
 from .config import resolve_data_config
 from .dataset import Dataset, DatasetTar
 from .transforms import *
-from .loader import create_loader
+from .loader import create_loader, create_transform
 from .mixup import mixup_target, FastCollateMixup
