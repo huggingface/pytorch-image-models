64966f61f70f64c44205b0c6b0944b268de22278
Ross Wightman <rwightman@gmail.com>
2019-08-29 15:21:38 -0700

    Add Nvidia's NovogGrad impl from Jasper (cleaner/faster than current) and Apex Fused optimizers

--- timm/optim/__init__.py
+++ timm/optim/__init__.py
@@ -3,5 +3,6 @@ from .rmsprop_tf import RMSpropTF
 from .adamw import AdamW
 from .radam import RAdam
 from .novograd import NovoGrad
+from .nvnovograd import NvNovoGrad
 from .lookahead import Lookahead
 from .optim_factory import create_optimizer
--- timm/optim/optim_factory.py
+++ timm/optim/optim_factory.py
@@ -1,5 +1,11 @@
+import torch
 from torch import optim as optim
-from timm.optim import Nadam, RMSpropTF, AdamW, RAdam, NovoGrad, Lookahead
+from timm.optim import Nadam, RMSpropTF, AdamW, RAdam, NovoGrad, NvNovoGrad, Lookahead
+try:
+    from apex.optimizers import FusedNovoGrad, FusedAdam, FusedLAMB, FusedSGD
+    has_apex = True
+except ImportError:
+    has_apex = False
 
 
 def add_weight_decay(model, weight_decay=1e-5, skip_list=()):
@@ -20,9 +26,10 @@ def add_weight_decay(model, weight_decay=1e-5, skip_list=()):
 def create_optimizer(args, model, filter_bias_and_bn=True):
     opt_lower = args.opt.lower()
     weight_decay = args.weight_decay
-    if opt_lower == 'adamw' or opt_lower == 'radam':
-        # compensate for the way current AdamW and RAdam optimizers
-        # apply the weight-decay
+    if 'adamw' in opt_lower or 'radam' in opt_lower:
+        # Compensate for the way current AdamW and RAdam optimizers apply LR to the weight-decay
+        # I don't believe they follow the paper or original Torch7 impl which schedules weight
+        # decay based on the ratio of current_lr/initial_lr
         weight_decay /= args.lr
     if weight_decay and filter_bias_and_bn:
         parameters = add_weight_decay(model, weight_decay)
@@ -30,12 +37,14 @@ def create_optimizer(args, model, filter_bias_and_bn=True):
     else:
         parameters = model.parameters()
 
+    if 'fused' in opt_lower:
+        assert has_apex and torch.cuda.is_available(), 'APEX and CUDA required for fused optimizers'
+
     opt_split = opt_lower.split('_')
     opt_lower = opt_split[-1]
     if opt_lower == 'sgd':
         optimizer = optim.SGD(
-            parameters, lr=args.lr,
-            momentum=args.momentum, weight_decay=weight_decay, nesterov=True)
+            parameters, lr=args.lr, momentum=args.momentum, weight_decay=weight_decay, nesterov=True)
     elif opt_lower == 'adam':
         optimizer = optim.Adam(
             parameters, lr=args.lr, weight_decay=weight_decay, eps=args.opt_eps)
@@ -61,6 +70,22 @@ def create_optimizer(args, model, filter_bias_and_bn=True):
             momentum=args.momentum, weight_decay=weight_decay)
     elif opt_lower == 'novograd':
         optimizer = NovoGrad(parameters, lr=args.lr, weight_decay=weight_decay, eps=args.opt_eps)
+    elif opt_lower == 'nvnovograd':
+        optimizer = NvNovoGrad(parameters, lr=args.lr, weight_decay=weight_decay, eps=args.opt_eps)
+    elif opt_lower == 'fusedsgd':
+        optimizer = FusedSGD(
+            parameters, lr=args.lr, momentum=args.momentum, weight_decay=weight_decay, nesterov=True)
+    elif opt_lower == 'fusedadam':
+        optimizer = FusedAdam(
+            parameters, lr=args.lr, adam_w_mode=False, weight_decay=weight_decay, eps=args.opt_eps)
+    elif opt_lower == 'fusedadamw':
+        optimizer = FusedAdam(
+            parameters, lr=args.lr, adam_w_mode=True, weight_decay=weight_decay, eps=args.opt_eps)
+    elif opt_lower == 'fusedlamb':
+        optimizer = FusedLAMB(parameters, lr=args.lr, weight_decay=weight_decay, eps=args.opt_eps)
+    elif opt_lower == 'fusednovograd':
+        optimizer = FusedNovoGrad(
+            parameters, lr=args.lr, betas=(0.95, 0.98), weight_decay=weight_decay, eps=args.opt_eps)
     else:
         assert False and "Invalid optimizer"
         raise ValueError
