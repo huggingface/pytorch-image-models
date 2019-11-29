5a0a8de7e376df850c018131e916de1f57f89d0e
Ross Wightman <rwightman@gmail.com>
2019-11-28 16:46:21 -0800

    ResNet updates: * remove redundant GluonResNet model/blocks and use the code in ResNet for Gluon weights * change SEModules back to using AdaptiveAvgPool instead of mean, PyTorch issue long fixed

--- timm/models/senet.py
+++ timm/models/senet.py
@@ -68,7 +68,7 @@ class SEModule(nn.Module):
 
     def __init__(self, channels, reduction):
         super(SEModule, self).__init__()
-        #self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Conv2d(
             channels, channels // reduction, kernel_size=1, padding=0)
         self.relu = nn.ReLU(inplace=True)
@@ -78,8 +78,7 @@ class SEModule(nn.Module):
 
     def forward(self, x):
         module_input = x
-        #x = self.avg_pool(x)
-        x = x.view(x.size(0), x.size(1), -1).mean(-1).view(x.size(0), x.size(1), 1, 1)
+        x = self.avg_pool(x)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
