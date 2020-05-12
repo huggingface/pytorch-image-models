5bd1ad13e714f3b4111e9356ab87f232f8fce863
Ross Wightman <rwightman@gmail.com>
2020-05-12 13:07:46 -0700

    Refactor test indent

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -8,12 +8,12 @@ from timm import list_models, create_model
 @pytest.mark.parametrize('model_name', list_models(exclude_filters='*efficientnet_l2*'))
 @pytest.mark.parametrize('batch_size', [1])
 def test_model_forward(model_name, batch_size):
-  """Run a single forward pass with each model"""
-  model = create_model(model_name, pretrained=False)
-  model.eval()
+    """Run a single forward pass with each model"""
+    model = create_model(model_name, pretrained=False)
+    model.eval()
 
-  inputs = torch.randn((batch_size, *model.default_cfg['input_size']))
-  outputs = model(inputs)
+    inputs = torch.randn((batch_size, *model.default_cfg['input_size']))
+    outputs = model(inputs)
 
-  assert outputs.shape[0] == batch_size
-  assert not torch.isnan(outputs).any(), 'Output included NaNs'
+    assert outputs.shape[0] == batch_size
+    assert not torch.isnan(outputs).any(), 'Output included NaNs'
