3bf15b91ed6a3dfd4429307292894fe366c1c79e
Sangdoo Yun <oodgnas@gmail.com>
2020-07-25 12:31:39 +0900

    Update __init__.py

--- timm/optim/__init__.py
+++ timm/optim/__init__.py
@@ -6,5 +6,5 @@ from .novograd import NovoGrad
 from .nvnovograd import NvNovoGrad
 from .lookahead import Lookahead
 from .adamp import AdamP
-from .adamp import SGDP
+from .sgdp import SGDP
 from .optim_factory import create_optimizer
