0167f749d31e301a095a9303b5a5df77872a38a6
Ross Wightman <rwightman@gmail.com>
2021-01-03 11:24:16 -0800

    Remove some old __future__ imports

--- timm/data/dataset.py
+++ timm/data/dataset.py
@@ -2,10 +2,6 @@
 
 Hacked together by / Copyright 2020 Ross Wightman
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import torch.utils.data as data
 
 import os
