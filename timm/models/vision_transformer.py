28c0fa31fea5c503be63d79f74d8dca82c41da32
Zhiyuan Chen <this@zyc.ai>
2021-01-18 18:13:01 +0800

    Update vision_transformer.py

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -136,6 +136,7 @@ class Block(nn.Module):
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.dropout = nn.Dropout(p=drop)
 
     def forward(self, x):
         residual = x.clone()
