94ca140b67cb602ee7e146af32bfb63b60df96f4
Reuben <reuben@Reubens-Air.gateway>
2021-02-10 23:54:35 +1100

    update collections.abc import

--- timm/models/layers/helpers.py
+++ timm/models/layers/helpers.py
@@ -3,13 +3,13 @@
 Hacked together by / Copyright 2020 Ross Wightman
 """
 from itertools import repeat
-from torch._six import container_abcs
+import collections.abc
 
 
 # From PyTorch internals
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, container_abcs.Iterable):
+        if isinstance(x, collections.abc.Iterable):
             return x
         return tuple(repeat(x, n))
     return parse
