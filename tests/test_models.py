809271b0f3e5d9be4e11c0c5cec1dbba8b5e2c60
contrastive <contrastive2021@163.com>
2021-03-29 09:39:50 +0800

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,7 +14,7 @@ if hasattr(torch._C, '_jit_set_profiling_executor'):
     torch._C._jit_set_profiling_mode(False)
 
 # transformer models don't support many of the spatial / feature based model functionalities
-NON_STD_FILTERS = ['vit_*']
+NON_STD_FILTERS = ['vit_*', 'tnt_*']
 
 # exclude models that cause specific test failures
 if 'GITHUB_ACTIONS' in os.environ:  # and 'Linux' in platform.system():
