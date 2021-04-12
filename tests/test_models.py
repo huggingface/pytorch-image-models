f89bf2c75229cd626799f8cdc688b45dfb8eb1aa
Aman Arora <aman.arora0210@gmail.com>
2021-04-12 02:55:12 -0400

    Add to exclude ftrs

--- tests/test_models.py
+++ tests/test_models.py
@@ -21,7 +21,7 @@ NUM_NON_STD = len(NON_STD_FILTERS)
 if 'GITHUB_ACTIONS' in os.environ:  # and 'Linux' in platform.system():
     # GitHub Linux runner is slower and hits memory limits sooner than MacOS, exclude bigger models
     EXCLUDE_FILTERS = [
-        '*efficientnet_l2*', '*resnext101_32x48d', '*in21k', '*152x4_bitm', 'resnetv2_101x1_bitm'
+        '*efficientnet_l2*', '*resnext101_32x48d', '*in21k', '*152x4_bitm'
         '*nfnet_f3*', '*nfnet_f4*', '*nfnet_f5*', '*nfnet_f6*', '*nfnet_f7*'] + NON_STD_FILTERS
 else:
     EXCLUDE_FILTERS = NON_STD_FILTERS
@@ -168,7 +168,7 @@ EXCLUDE_FEAT_FILTERS = [
 ]
 if 'GITHUB_ACTIONS' in os.environ:  # and 'Linux' in platform.system():
     # GitHub Linux runner is slower and hits memory limits sooner than MacOS, exclude bigger models
-    EXCLUDE_FEAT_FILTERS += ['*resnext101_32x32d', '*resnext101_32x16d']
+    EXCLUDE_FEAT_FILTERS += ['*resnext101_32x32d', '*resnext101_32x16d', 'resnetv2_101x1_bitm']
 
 
 @pytest.mark.timeout(210)
