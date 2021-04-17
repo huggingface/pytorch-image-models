a04427d8ce4a6ea01510452eba0ed27a83c1d31b
Ross Wightman <rwightman@gmail.com>
2021-04-17 12:17:45 -0700

    Add _in22k to bulk validate filter

--- validate.py
+++ validate.py
@@ -289,7 +289,7 @@ def main():
         if args.model == 'all':
             # validate all models in a list of names with pretrained checkpoints
             args.pretrained = True
-            model_names = list_models(pretrained=True, exclude_filters=['*in21k'])
+            model_names = list_models(pretrained=True, exclude_filters=['*_in21k', '*_in22k'])
             model_cfgs = [(n, '') for n in model_names]
         elif not is_model(args.model):
             # model name doesn't exist, try as wildcard filter
