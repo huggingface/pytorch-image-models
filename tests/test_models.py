163331748935559923ffb6aa5ed1882b47a6a92a
Aman Arora <aman.arora0210@gmail.com>
2021-05-21 01:11:56 +0000

    update tests and exclude convit_base

--- tests/test_models.py
+++ tests/test_models.py
@@ -24,7 +24,7 @@ if 'GITHUB_ACTIONS' in os.environ:  # and 'Linux' in platform.system():
     EXCLUDE_FILTERS = [
         '*efficientnet_l2*', '*resnext101_32x48d', '*in21k', '*152x4_bitm', '*101x3_bitm',
         '*nfnet_f3*', '*nfnet_f4*', '*nfnet_f5*', '*nfnet_f6*', '*nfnet_f7*', 
-        '*resnetrs350*', '*resnetrs420*'] + NON_STD_FILTERS
+        '*resnetrs350*', '*resnetrs420*', 'convit_base'] + NON_STD_FILTERS
 else:
     EXCLUDE_FILTERS = NON_STD_FILTERS
 
