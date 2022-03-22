e1e037ba52a5c18068fb9f0392a8895f760c2676
Ross Wightman <rwightman@gmail.com>
2022-03-21 22:00:33 -0700

    Fix bad tuple typing fix that was on XLA branch bust missed on master merge

--- timm/models/swin_transformer_v2_cr.py
+++ timm/models/swin_transformer_v2_cr.py
@@ -120,8 +120,8 @@ def window_partition(x, window_size: Tuple[int, int]):
 
 
 @register_notrace_function  # reason: int argument is a Proxy
-def window_reverse(windows, window_size: tuple[int, int], img_size: tuple[int, int]):
-    """
+def window_reverse(windows, window_size: Tuple[int, int], img_size: Tuple[int, int]):
+    """`
     Args:
         windows: (num_windows * B, window_size[0], window_size[1], C)
         window_size (Tuple[int, int]): Window size
