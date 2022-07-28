8ad4bdfa06022fbefefec19d15de2bd7bcd31a8e
Ross Wightman <rwightman@gmail.com>
2022-07-28 16:18:18 -0700

    Allow ntuple to be used with string values

--- timm/models/layers/helpers.py
+++ timm/models/layers/helpers.py
@@ -9,7 +9,7 @@ import collections.abc
 # From PyTorch internals
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, collections.abc.Iterable):
+        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
             return x
         return tuple(repeat(x, n))
     return parse
