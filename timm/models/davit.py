fa45868239ca2390f6b040c9b5d3fdba0a34f424
Fredo Guan <fredo.guan@hotmail.com>
2022-12-08 11:47:15 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -216,8 +216,8 @@ def window_reverse(windows : Tensor, window_size: int, H: int, W: int):
         x: (B, H, W, C)
     """
     
-    B : int = (windows.size(dim=0) / (H * W / window_size / window_size))
-    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    B : float = (windows.size(dim=0) / (H * W / window_size / window_size))
+    x = windows.view(int(B), H // window_size, W // window_size, window_size, window_size, -1)
     x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
     return x
 
