bbafe330cb04a91f21f757a3b22fed08c7e131df
Fredo Guan <fredo.guan@hotmail.com>
2022-12-08 06:13:34 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -453,7 +453,7 @@ class DaViT(nn.Module):
             self.feature_info += [dict(
                 num_chs=self.embed_dims[stage_id],
                 reduction = 2,
-                module=f'stages.stage_{stage_id}.{depths[stage_id] - 1}')]#.{len(attention_types) - 1}.mlp')]
+                module=f'stages.stage_{stage_id}.{depths[stage_id] - 1}'.{len(attention_types) - 1}.mlp')]
 
 
         self.norms = norm_layer(self.num_features)
