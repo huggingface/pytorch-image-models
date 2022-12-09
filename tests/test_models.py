3f7f6d8c16c286dd63bcb02f72f60100b6172511
Fredo Guan <fredo.guan@hotmail.com>
2022-12-08 20:09:48 -0800

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -28,7 +28,7 @@ NON_STD_FILTERS = [
     'convit_*', 'levit*', 'visformer*', 'deit*', 'jx_nest_*', 'nest_*', 'xcit_*', 'crossvit_*', 'beit*',
     'poolformer_*', 'volo_*', 'sequencer2d_*', 'swinv2_*', 'pvt_v2*', 'mvitv2*', 'gcvit*', 'efficientformer*',
 
-    'coatnet*', 'coatnext*', 'maxvit*', 'maxxvit*', 'davit*', 'eva_*'
+    'coatnet*', 'coatnext*', 'maxvit*', 'maxxvit*', 'eva_*'
 
 ]
 NUM_NON_STD = len(NON_STD_FILTERS)
