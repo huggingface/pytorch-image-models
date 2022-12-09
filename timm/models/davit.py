1fa1e01646092da9f756601467513d103192af2d
Fredo Guan <fredo.guan@hotmail.com>
2022-12-08 17:43:35 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -224,7 +224,11 @@ def window_reverse(windows : Tensor, window_size: int, H: int, W: int):
         window_size,
         window_size,
         -1)
-    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(
+        torch.floor(torch.tensor(windows.size(dim=0)) / (H * W / window_size / window_size)).int().item(),
+        H,
+        W,
+        -1)
     return x
 
 
