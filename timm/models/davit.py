5c73dd66cbed2fb5b51fef6da7a7100b076c1387
Fredo Guan <fredo.guan@hotmail.com>
2022-12-10 04:42:09 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -45,7 +45,7 @@ class SequentialWithSize(nn.Sequential):
 
 class SequentialWithSize(nn.Sequential):
     def __init__(self, *args, **kwargs):
-        super(SequentialWithSize, self).__init__(*args, **kwargs)
+        super(SequentialWithSize, nn.Sequential, self).__init__(*args, **kwargs)
         
     def forward(self, x : Tensor, size: Tuple[int, int]):
         for module in self._modules.values():
