37ab7fe2e21c3be2f24ec1d8ec02d715caaa200d
Fredo Guan <fredo.guan@hotmail.com>
2023-01-07 20:31:57 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -595,7 +595,7 @@ def _cfg(url='', **kwargs):
         'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': (7, 7),
         'crop_pct': 0.875, 'interpolation': 'bilinear',
         'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD,
-        'first_conv': 'patch_embed.proj', 'classifier': 'head.fc',
+        'first_conv': 'stem.proj', 'classifier': 'head.fc',
         **kwargs
     }
 
