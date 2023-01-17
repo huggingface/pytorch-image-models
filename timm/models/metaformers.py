e4f89b2e25064fb4368a83cc5bc2d916f6f3a2cf
Fredo Guan <fredo.guan@hotmail.com>
2023-01-17 12:33:31 -0800

    Revert "Update metaformers.py"
    This reverts commit 2916f37f8d862cd4fcbc86c858b23894daec3339.

--- timm/models/metaformers.py
+++ timm/models/metaformers.py
@@ -816,10 +816,8 @@ class MetaFormer(nn.Module):
         
     def forward_features(self, x):
         x = self.patch_embed(x)
-        #x = self.stages(x)
-        for i, stage in enumerate(self.stages):
-            x = stage(x)
-            print(x[0][0][0][0])
+        x = self.stages(x)
+        
         
         return x 
 
