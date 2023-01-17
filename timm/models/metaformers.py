2916f37f8d862cd4fcbc86c858b23894daec3339
Fredo Guan <fredo.guan@hotmail.com>
2023-01-17 12:27:12 -0800

    Update metaformers.py

--- timm/models/metaformers.py
+++ timm/models/metaformers.py
@@ -816,8 +816,10 @@ class MetaFormer(nn.Module):
         
     def forward_features(self, x):
         x = self.patch_embed(x)
-        x = self.stages(x)
-        
+        #x = self.stages(x)
+        for i, stage in enumerate(self.stages):
+            x = stage(x)
+            print(x[0][0][0][0])
         
         return x 
 
