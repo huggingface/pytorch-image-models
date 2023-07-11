c5a24bff019ce8cb6835ec5b9a0ce95c2170e2af
Nudavia <18207105141@163.com>
2023-07-11 16:40:02 +0800

    add patchmix and test

--- timm/data/patchmix.py
+++ timm/data/patchmix.py
@@ -10,7 +10,8 @@ import torch
 import random
 import numpy as np
 
-def one_hot(x, num_classes, on_value=1., off_value=0.):
+
+def one_hot(x, num_classes, on_value=1.0, off_value=0.0):
     return torch.full((x.size()[0], num_classes), off_value, device=x.device).scatter_(1, x, on_value)
 
 
@@ -35,11 +36,8 @@ class PatchMix:
         patch_size (int): size of image patch. Default is ``16``.
         smoothing (float): coefficient of label smoothing Default is ``0.``.
     """
-    def __init__(self, num_classes,
-                 prob=0.5,
-                 mix_num=2,
-                 patch_size=16,
-                 smoothing=0.):
+
+    def __init__(self, num_classes, prob=0.5, mix_num=2, patch_size=16, smoothing=0.0):
         super().__init__()
         self.prob = prob
         self.mix_num = mix_num
@@ -50,11 +48,9 @@ class PatchMix:
     def _shuffle(self, x):
         b, l = x.shape[:2]
         indexes = random_indexes(l)
-        forward_indexes = torch.as_tensor(
-            indexes[0], dtype=torch.long).to(x.device)
+        forward_indexes = torch.as_tensor(indexes[0], dtype=torch.long).to(x.device)
         forward_indexes = forward_indexes.repeat(b, 1)
-        backward_indexes = torch.as_tensor(
-            indexes[1], dtype=torch.long).to(x.device)
+        backward_indexes = torch.as_tensor(indexes[1], dtype=torch.long).to(x.device)
         backward_indexes = backward_indexes.repeat(b, 1)
         x = take_indexes(x, forward_indexes)
         return x, forward_indexes, backward_indexes
@@ -62,7 +58,7 @@ class PatchMix:
     def _mix(self, x, m):
         b, l, c = x.shape
         s = l // m
-        d = b*m
+        d = b * m
         l_ = int(s * m)
         # get the image sequence that needs to be mixed, and drop the last.
         mix_x = x[:, :l_]
@@ -70,15 +66,13 @@ class PatchMix:
         # generate the mix index for mixing patch group.
         ids = torch.arange(d, device=x.device)
         mix_indexes = (ids + ids % m * m) % d
-        mix_x = torch.gather(
-            mix_x, 0, mix_indexes.repeat(s, c, 1).permute(-1, 0, 1))
+        mix_x = torch.gather(mix_x, 0, mix_indexes.repeat(s, c, 1).permute(-1, 0, 1))
         mix_x = mix_x.reshape(b, l_, c)
         x[:, :l_] = mix_x
         # generate the mix index for mixing target.
         ids = torch.arange(b, device=x.device).view(-1, 1)
         m2o_indexes = (ids + torch.arange(m, device=x.device)) % b
-        m2m_indexes = ((ids - m + 1) + torch.arange(m *
-                       2 - 1, device=x.device) + b) % b
+        m2m_indexes = ((ids - m + 1) + torch.arange(m * 2 - 1, device=x.device) + b) % b
         return x, m2o_indexes, m2m_indexes
 
     def __call__(self, x, target):
@@ -89,7 +83,7 @@ class PatchMix:
         Returns:
             x (Tensor): mixed image.
             m2o_target (Tensor): target between mixed images and original images in infoNCE loss.
-            m2m_target (Tensor): target between mixed images and mixed images in infoNCE loss. 
+            m2m_target (Tensor): target between mixed images and mixed images in infoNCE loss.
         """
         b, c, h, w = x.shape
         m = self.mix_num
@@ -100,14 +94,12 @@ class PatchMix:
             n_h = h // p
             n_w = w // p
             # b c (w p1) (h p2) -> b (w h) (c p1 p2)
-            x = x.reshape(b, c, n_h, p, n_w, p).permute(
-                0, 2, 4, 1, 3, 5).reshape(b, n_h*n_w, c*p*p)
+            x = x.reshape(b, c, n_h, p, n_w, p).permute(0, 2, 4, 1, 3, 5).reshape(b, n_h * n_w, c * p * p)
             x, _, backward_indexes = self._shuffle(x)
             x, m2o_indexes, m2m_indexes = self._mix(x, m)
             x = take_indexes(x, backward_indexes)
             # b (w h) (c p1 p2) -> b c (w p1) (h p2)
-            x = x.reshape(b, n_h, n_w, c, p, p).permute(
-                0, 3, 1, 4, 2, 5).reshape(b, c, n_h*p, n_w*p)
+            x = x.reshape(b, n_h, n_w, c, p, p).permute(0, 3, 1, 4, 2, 5).reshape(b, c, n_h * p, n_w * p)
         else:
             m = 1
             m2o_indexes = target.view(-1, 1)
@@ -123,9 +115,8 @@ class PatchMix:
         m2o_target = one_hot(m2o_target, self.num_classes, on_value, off_value)
 
         ids = torch.arange(m2m_target.shape[1], device=x.device)
-        weights = (1.0 - torch.abs(m - ids - 1) / m)
+        weights = 1.0 - torch.abs(m - ids - 1) / m
         on_value = (1.0 - self.smoothing) * weights / m + off_value
-        m2m_target = one_hot(m2m_target, self.num_classes, on_value.expand(
-            [m2m_target.shape[0], -1]), off_value)
+        m2m_target = one_hot(m2m_target, self.num_classes, on_value.expand([m2m_target.shape[0], -1]), off_value)
 
         return x, m2o_target, m2m_target
