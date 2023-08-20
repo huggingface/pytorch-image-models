92afd05a531c055eea14ea6f333fc5a5c6efee11
Ross Wightman <rwightman@gmail.com>
2023-08-20 11:54:20 -0700

    samvit head fc name fix

--- timm/models/vision_transformer_sam.py
+++ timm/models/vision_transformer_sam.py
@@ -584,7 +584,7 @@ def _cfg(url='', **kwargs):
         'num_classes': 1000, 'input_size': (3, 1024, 1024), 'pool_size': None,
         'crop_pct': .9, 'interpolation': 'bicubic', 'fixed_input_size': True,
         'mean': IMAGENET_INCEPTION_MEAN, 'std': IMAGENET_INCEPTION_STD,
-        'first_conv': 'patch_embed.proj', 'classifier': 'head',
+        'first_conv': 'patch_embed.proj', 'classifier': 'head.fc',
         **kwargs
     }
 
