e5dac98568acd7b89d11a0d99924af92b974eab1
mjamroz <jamroz@chem.uw.edu.pl>
2023-09-16 07:21:42 +0200

    remove module. prefix

--- timm/models/_helpers.py
+++ timm/models/_helpers.py
@@ -20,12 +20,8 @@ __all__ = ['clean_state_dict', 'load_state_dict', 'load_checkpoint', 'remap_stat
 
 
 def clean_state_dict(state_dict: Dict[str, Any]) -> Dict[str, Any]:
-    # 'clean' checkpoint by removing .module prefix from state dict if it exists from parallel training
-    cleaned_state_dict = {}
-    for k, v in state_dict.items():
-        name = k[7:] if k.startswith('module.') else k
-        cleaned_state_dict[name] = v
-    return cleaned_state_dict
+    # 'clean' checkpoint by removing module. prefix from state dict if it exists from parallel training
+    return {k.removeprefix('module.'): v for k,v in state_dict.items()
 
 
 def load_state_dict(
