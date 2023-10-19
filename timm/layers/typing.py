cefa268c315fbadc4dfdfccd44793dcc20fcd101
a-r-r-o-w <avs050602@gmail.com>
2023-10-20 02:47:27 +0530

    include typing suggestions by @rwightman

--- timm/layers/typing.py
+++ timm/layers/typing.py
@@ -1,10 +1,9 @@
 import functools
 import types
-from typing import Any, Dict, List, Tuple, Union
+from typing import Tuple, Union
 
 import torch.nn
 
 
-BlockArgs = List[List[Dict[str, Any]]]
 LayerType = Union[type, str, types.FunctionType, functools.partial, torch.nn.Module]
 PadType = Union[str, int, Tuple[int, int]]
