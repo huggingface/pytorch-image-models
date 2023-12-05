c0598ad82a9e334652fc6488900efce5b22bf104
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2023-12-05 11:13:17 +0900

    fix doc position

--- timm/models/swin_transformer.py
+++ timm/models/swin_transformer.py
@@ -383,8 +383,8 @@ class SwinTransformerStage(nn.Module):
         """
         Args:
             dim: Number of input channels.
-            input_resolution: Input resolution.
             out_dim: Number of output channels.
+            input_resolution: Input resolution.
             depth: Number of blocks.
             downsample: Downsample layer at the end of the layer.
             num_heads: Number of attention heads.
