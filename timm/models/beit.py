2fa4ef67a39612089d146fb1b4c52e213f68d475
Fredo Guan <fredo.guan@hotmail.com>
2023-12-08 13:33:02 -0700

    Update beit.py

--- timm/models/beit.py
+++ timm/models/beit.py
@@ -800,7 +800,7 @@ class Beit(nn.Module):
                 x = checkpoint(blk, x, shared_rel_pos_bias=rel_pos_bias)
             else:
                 x = blk(x, shared_rel_pos_bias=rel_pos_bias)
-        x = self.norm(x)
+        #x = self.norm(x)
         return x
 
     def forward_head(self, x: torch.Tensor, pre_logits: bool = False) -> torch.Tensor:
