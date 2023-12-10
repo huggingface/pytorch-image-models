e7661e9712190fea0b4b0b346cad138ff8af5c45
Fredo Guan <fredo.guan@hotmail.com>
2023-12-10 15:23:38 -0700

    Update edgenext.py

--- timm/models/edgenext.py
+++ timm/models/edgenext.py
@@ -416,7 +416,6 @@ class EdgeNeXt(nn.Module):
         return self.head.fc
 
     def reset_classifier(self, num_classes: int, global_pool: Optional[str] = None):
-        self.num_classes = num_classes
         self.head.reset(num_classes, global_pool)
 
     def forward_features(self, x):
