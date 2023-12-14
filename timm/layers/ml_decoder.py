e6825bbf0e8c20e0091dee2da44ebdf24a593da5
Fredo Guan <fredo.guan@hotmail.com>
2023-12-13 23:36:58 -0800

    Update ml_decoder.py

--- timm/layers/ml_decoder.py
+++ timm/layers/ml_decoder.py
@@ -159,12 +159,9 @@ class MLDecoder(nn.Module):
         tgt = query_embed.unsqueeze(1).expand(-1, bs, -1)  # no allocation of memory with expand
         h = self.decoder(tgt, embedding_spatial_786.transpose(0, 1))  # [embed_len_decoder, batch, 768]
         h = h.transpose(0, 1)
-
-        out_extrap = torch.zeros(h.shape[0], h.shape[1], self.duplicate_factor, device=h.device, dtype=h.dtype)
-        for i in range(self.embed_len_decoder):  # group FC
-            h_i = h[:, i, :]
-            w_i = self.duplicate_pooling[i, :, :]
-            out_extrap[:, i, :] = torch.matmul(h_i, w_i)
+        #out_extrap = torch.zeros(h.shape[0], h.shape[1], self.duplicate_factor, device=h.device, dtype=h.dtype)
+        #self.group_fc(h, self.duplicate_pooling, out_extrap)
+        out_extrap = (h.permute(1, 0, 2) @ duplicate_pooling).permute(1,0,2) # [B, K, N/K]
         h_out = out_extrap.flatten(1)[:, :self.num_classes]
         h_out += self.duplicate_pooling_bias
         logits = h_out
