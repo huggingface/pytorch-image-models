3194117d64977b5e451d80fe999fa0c6f10e5413
Fredo Guan <fredo.guan@hotmail.com>
2023-12-16 22:44:51 -0800

    Update ml_decoder.py

--- timm/layers/ml_decoder.py
+++ timm/layers/ml_decoder.py
@@ -177,7 +177,7 @@ class GroupLinear(nn.Module):
         nn.init.xavier_normal_(self.weight)
         nn.init.constant_(self.bias, 0)
     
-    def forward(self, x):
+    def forward(self, x): # [B, K, C]
         x = (x @ self.weight).permute(1, 0, 2).flatten(1)[:, :self.num_classes]
         x += self.bias
         return x
@@ -214,7 +214,7 @@ class MLDecoder(nn.Module):
         self.norm1 = norm_layer(dim)
         
         
-        self.attn = nn.MultiheadAttention(dim, num_heads, dropout=attn_drop)
+        self.attn = CrossAttention(dim, num_heads=num_heads)
         self.norm2 = norm_layer(dim)
         self.mlp = Mlp(
             in_features=dim,
@@ -231,12 +231,11 @@ class MLDecoder(nn.Module):
                 
         x = self.act(self.proj(x))
         q = self.embed_norm(self.embed_drop(self.query_embed.weight))
-        xN = self.norm1(x)
-        x = x + self.attn(q, xN, xN)[0]
+        x = x + self.attn(q, self.norm1(x))
         x = x + self.mlp(self.norm2(x))
         x = self.fc(x)
         
-'''
+
             
 class CrossAttention(nn.Module):
     fused_attn: Final[bool]
@@ -247,8 +246,8 @@ class CrossAttention(nn.Module):
             num_heads: int = 8,
             qkv_bias: bool = True,
             qk_norm: bool = False,
-            attn_drop: float = 0.,
-            proj_drop: float = 0.,
+            attn_drop: float = 0.1,
+            proj_drop: float = 0.1,
             norm_layer: nn.Module = nn.LayerNorm,
     ) -> None:
         super().__init__()
@@ -267,9 +266,11 @@ class CrossAttention(nn.Module):
         self.proj_drop = nn.Dropout(proj_drop)
 
     def forward(self, q, x) -> torch.Tensor:
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv.unbind(0)
+        K, _ = q.shape # [K, C]
+        B, N, C = x.shape # [B, N, C]
+        q = self.q(q).reshape(1, K, self.num_heads, self.head_dim).permute(0, 2, 1, 3) # [1, n_h, K, d_h]
+        kv = self.kv(x).reshape(B, N, 2, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4) # [2, B, n_h, N, d_h]
+        k, v = kv.unbind(0)
         q, k = self.q_norm(q), self.k_norm(k)
 
         if self.fused_attn:
@@ -279,32 +280,15 @@ class CrossAttention(nn.Module):
             )
         else:
             q = q * self.scale
-            attn = q @ k.transpose(-2, -1)
+            attn = q @ k.transpose(-2, -1) # [B, n_h, K, N]
             attn = attn.softmax(dim=-1)
             attn = self.attn_drop(attn)
-            x = attn @ v
+            x = attn @ v # [B, n_h, K, d_h]
 
-        x = x.transpose(1, 2).reshape(B, N, C)
+        x = x.transpose(1, 2).reshape(B, K, C)
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
         
-        q = self.embed_norm(self.embed_drop(self.query_embed.weight))
-        q = self.q(q).reshape(B, N, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
-        else:
-            kv = self.kv(x).reshape(B, N, 2, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-            k, v = kv.unbind(0)
+
         
-        if self.fused_attn:
-            x = F.scaled_dot_product_attention(
-                q, k, v,
-                dropout_p=self.attn_drop.p if self.training else 0.,
-            )
-        else:
-            q = q * self.scale
-            attn = q @ k.transpose(-2, -1)
-            attn = attn.softmax(dim=-1)
-            attn = self.attn_drop(attn)
-            x = attn @ v
-        
-'''
