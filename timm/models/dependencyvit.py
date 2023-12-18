87590485bffc07d3c2ee268e4572b3d13e8ef9dd
Fredo Guan <fredo.guan@hotmail.com>
2023-12-18 03:10:02 -0800

    Update dependencyvit.py

--- timm/models/dependencyvit.py
+++ timm/models/dependencyvit.py
@@ -75,7 +75,7 @@ class ReversedAttention(nn.Module):
         q, k, v = qkv.unbind(0)
         q, k = self.q_norm(q), self.k_norm(k)
 
-        p = (self.head_selector(x) / self.head_selector_temperature).softmax(dim=-1)
+        p = (self.head_selector(x) * self.head_selector_temperature).softmax(dim=-1)
         p = p.transpose(-2, -1).reshape(B, self.num_heads, 1, N)
 
         m = m * self.message_controller(x).sigmoid().reshape(B, 1, 1, N)
@@ -184,7 +184,7 @@ class DependencyViT(VisionTransformer):
         else:
             x, m = self.blocks((x, m))
         x = self.norm(x)
-        #x = x * m.transpose(1, 3).squeeze(-1)
+        x = x * m.transpose(1, 3).squeeze(-1)
         return x
 
 
