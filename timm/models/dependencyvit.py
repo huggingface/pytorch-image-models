c9d1666644140baeb58f5e6be35475ab93e2658d
Fredo Guan <fredo.guan@hotmail.com>
2023-12-18 03:20:11 -0800

    Update dependencyvit.py

--- timm/models/dependencyvit.py
+++ timm/models/dependencyvit.py
@@ -184,7 +184,7 @@ class DependencyViT(VisionTransformer):
         else:
             x, m = self.blocks((x, m))
         x = self.norm(x)
-        x = x * m.transpose(1, 3).squeeze(-1) # FIXME before or after norm
+        #x = x * m.transpose(1, 3).squeeze(-1) # FIXME before or after norm
         return x
 
 
