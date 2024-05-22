e748805be31318da1a0e34b61294704666f50397
Ross Wightman <rwightman@gmail.com>
2024-05-22 14:33:39 -0700

    Add regex matching support to AttentionExtract. Add return_dict support to graph extractors and use returned output in AttentionExtractor

--- timm/utils/attention_extract.py
+++ timm/utils/attention_extract.py
@@ -1,4 +1,5 @@
 import fnmatch
+import re
 from collections import OrderedDict
 from typing import Union, Optional, List
 
@@ -17,6 +18,7 @@ class AttentionExtract(torch.nn.Module):
             mode: str = 'eval',
             method: str = 'fx',
             hook_type: str = 'forward',
+            use_regex: bool = False,
     ):
         """ Extract attention maps (or other activations) from a model by name.
 
@@ -26,6 +28,7 @@ class AttentionExtract(torch.nn.Module):
             mode: 'train' or 'eval' model mode.
             method: 'fx' or 'hook' extraction method.
             hook_type: 'forward' or 'forward_pre' hooks used.
+            use_regex: Use regex instead of fnmatch
         """
         super().__init__()
         assert mode in ('train', 'eval')
@@ -41,11 +44,15 @@ class AttentionExtract(torch.nn.Module):
 
             node_names = get_graph_node_names(model)[0 if mode == 'train' else 1]
             names = names or self.default_node_names
-            matched = [g for g in node_names if any([fnmatch.fnmatch(g, n) for n in names])]
+            if use_regex:
+                regexes = [re.compile(r) for r in names]
+                matched = [g for g in node_names if any([r.match(g) for r in regexes])]
+            else:
+                matched = [g for g in node_names if any([fnmatch.fnmatch(g, n) for n in names])]
             if not matched:
                 raise RuntimeError(f'No node names found matching {names}.')
 
-            self.model = GraphExtractNet(model, matched)
+            self.model = GraphExtractNet(model, matched, return_dict=True)
             self.hooks = None
         else:
             # names are module names
@@ -54,7 +61,11 @@ class AttentionExtract(torch.nn.Module):
 
             module_names = [n for n, m in model.named_modules()]
             names = names or self.default_module_names
-            matched = [m for m in module_names if any([fnmatch.fnmatch(m, n) for n in names])]
+            if use_regex:
+                regexes = [re.compile(r) for r in names]
+                matched = [m for m in module_names if any([r.match(m) for r in regexes])]
+            else:
+                matched = [m for m in module_names if any([fnmatch.fnmatch(m, n) for n in names])]
             if not matched:
                 raise RuntimeError(f'No module names found matching {names}.')
 
@@ -71,5 +82,4 @@ class AttentionExtract(torch.nn.Module):
             output = self.hooks.get_output(device=x.device)
         else:
             output = self.model(x)
-            output = OrderedDict(zip(self.names, output))
         return output
