ec857fc90b366a6fd10b2c08cd1e22ee16e23406
Ross Wightman <rwightman@gmail.com>
2024-11-08 09:10:17 -0800

    Add ADOPT optimizer

--- timm/optim/__init__.py
+++ timm/optim/__init__.py
@@ -1,12 +1,14 @@
 from .adabelief import AdaBelief
 from .adafactor import Adafactor
+from .adafactor_bv import AdafactorBigVision
 from .adahessian import Adahessian
 from .adamp import AdamP
 from .adamw import AdamW
 from .adan import Adan
-from .adafactor_bv import AdafactorBigVision
+from .adopt import Adopt
 from .lamb import Lamb
 from .lars import Lars
+from .lion import Lion
 from .lookahead import Lookahead
 from .madgrad import MADGRAD
 from .nadam import Nadam
@@ -14,5 +16,5 @@ from .nvnovograd import NvNovoGrad
 from .radam import RAdam
 from .rmsprop_tf import RMSpropTF
 from .sgdp import SGDP
-from .lion import Lion
+
 from .optim_factory import create_optimizer, create_optimizer_v2, optimizer_kwargs
