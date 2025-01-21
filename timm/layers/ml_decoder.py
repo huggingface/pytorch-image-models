bd6a20ae47e2a4ba2dc2a8b5abc862111eed3737
Fredo Guan <fredo.guan@hotmail.com>
2025-01-21 01:38:35 -0700

    monkeypatch ml-decoder + metaformer

--- timm/layers/ml_decoder.py
+++ timm/layers/ml_decoder.py
@@ -82,8 +82,9 @@ def add_ml_decoder_head(model, head_version='new', **kwargs):
         del model.head.fc
         model.head.fc = head
         '''
+        target_type = type(model.forward_head)
         model.head = MLDecoderHead(head, num_features, num_classes)
-        model.forward_head = model.head.forward
+        model.forward_head = target_type(model.head.forward, model, type(model))
 
     # maybe  and isinstance(model.head, (NormMlpClassifierHead, ClassifierHead) ?
     elif hasattr(model, 'head'):    # ClassifierHead, nn.Sequential
