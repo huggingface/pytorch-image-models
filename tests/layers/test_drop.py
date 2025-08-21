a7a3186f8f861d7ecf095543a7d3bf6fe65d6792
Crutcher Dunnavant <crutcher@gmail.com>
2025-08-21 10:16:51 -0700

    format

--- tests/layers/test_drop.py
+++ tests/layers/test_drop.py
@@ -6,10 +6,11 @@ import torch
 
 from timm.layers import drop
 
-torch_backend = os.environ.get('TORCH_BACKEND')
+torch_backend = os.environ.get("TORCH_BACKEND")
 if torch_backend is not None:
     importlib.import_module(torch_backend)
-torch_device = os.environ.get('TORCH_DEVICE', 'cpu')
+torch_device = os.environ.get("TORCH_DEVICE", "cpu")
+
 
 class Conv2dKernelMidpointMask2d(unittest.TestCase):
     def test_conv2d_kernel_midpoint_mask_odd(self):
@@ -21,15 +22,13 @@ class Conv2dKernelMidpointMask2d(unittest.TestCase):
         )
         print(mask)
         assert mask.device == torch.device(torch_device)
-        assert mask.tolist() == \
-            [
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            ]
-
+        assert mask.tolist() == [
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ]
 
     def test_conv2d_kernel_midpoint_mask_even(self):
         mask = drop.conv2d_kernel_midpoint_mask(
@@ -40,14 +39,13 @@ class Conv2dKernelMidpointMask2d(unittest.TestCase):
         )
         print(mask)
         assert mask.device == torch.device(torch_device)
-        assert mask.tolist() == \
-            [
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-                [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-            ]
+        assert mask.tolist() == [
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        ]
 
     def test_clip_mask_2d_kernel_too_big(self):
         try:
@@ -65,67 +63,70 @@ class Conv2dKernelMidpointMask2d(unittest.TestCase):
 
 class DropBlock2dDropFilterTest(unittest.TestCase):
     def test_drop_filter(self):
-        selection = torch.tensor(
-            [
-                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-            ],
-            device=torch_device,
-        ).unsqueeze(0).unsqueeze(0)
+        selection = (
+            torch.tensor(
+                [
+                    [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                ],
+                device=torch_device,
+            )
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )
 
         result = drop.drop_block_2d_drop_filter_(
-            selection=selection,
-            kernel=(2, 3),
-            partial_edge_blocks=False
+            selection=selection, kernel=(2, 3), partial_edge_blocks=False
         ).squeeze()
         print(result)
         assert result.device == torch.device(torch_device)
-        assert result.tolist() == \
-            [
-                [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
-                [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            ]
+        assert result.tolist() == [
+            [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ]
 
     def test_drop_filter_partial_edge_blocks(self):
-        selection = torch.tensor(
-            [
-                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-            ],
-            device=torch_device,
-            dtype=torch.float32,
-        ).unsqueeze(0).unsqueeze(0)
+        selection = (
+            torch.tensor(
+                [
+                    [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                ],
+                device=torch_device,
+                dtype=torch.float32,
+            )
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )
 
         result = drop.drop_block_2d_drop_filter_(
-            selection=selection,
-            kernel=(2, 3),
-            partial_edge_blocks=True
+            selection=selection, kernel=(2, 3), partial_edge_blocks=True
         ).squeeze()
         print(result)
         assert result.device == torch.device(torch_device)
-        assert result.tolist() == \
-            [
-                [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0],
-                [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
-            ]
+        assert result.tolist() == [
+            [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+        ]
+
 
 class DropBlock2dTest(unittest.TestCase):
     def test_drop_block_2d(self):
         tensor = torch.ones((1, 1, 200, 300), device=torch_device)
 
-        drop_prob=0.1
+        drop_prob = 0.1
         keep_prob = 1.0 - drop_prob
 
         result = drop.drop_block_2d(
@@ -138,6 +139,6 @@ class DropBlock2dTest(unittest.TestCase):
         unchanged = float(len(result[result == 1.0]))
         keep_ratio = unchanged / numel
 
-        assert abs(keep_ratio - keep_prob) < 0.05, \
-                f"abs({keep_ratio=} - {keep_prob=}) ! < 0.05"
-
+        assert (
+            abs(keep_ratio - keep_prob) < 0.05
+        ), f"abs({keep_ratio=} - {keep_prob=}) ! < 0.05"
