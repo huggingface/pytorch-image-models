f9afb840176df0c6d95017a0f4aaae2930df5bb4
Ross Wightman <rwightman@gmail.com>
2025-09-28 16:17:39 -0700

    Fix typo for s2d norm

--- timm/models/_efficientnet_blocks.py
+++ timm/models/_efficientnet_blocks.py
@@ -177,7 +177,7 @@ class DepthwiseSeparableConv(nn.Module):
         if s2d == 1:
             sd_chs = int(in_chs * 4)
             self.conv_s2d = create_conv2d(in_chs, sd_chs, kernel_size=2, stride=2, padding='same', **dd)
-            self.bn_s2d = norm_act_layer(sd_ch, **dds)
+            self.bn_s2d = norm_act_layer(sd_chs, **dd)
             dw_kernel_size = (dw_kernel_size + 1) // 2
             dw_pad_type = 'same' if dw_kernel_size == 2 else pad_type
             in_chs = sd_chs
@@ -275,7 +275,7 @@ class InvertedResidual(nn.Module):
         if s2d == 1:
             sd_chs = int(in_chs * 4)
             self.conv_s2d = create_conv2d(in_chs, sd_chs, kernel_size=2, stride=2, padding='same', **dd)
-            self.bn_s2d = norm_act_layer(sd_chs, sd_chs, **dd)
+            self.bn_s2d = norm_act_layer(sd_chs, **dd)
             dw_kernel_size = (dw_kernel_size + 1) // 2
             dw_pad_type = 'same' if dw_kernel_size == 2 else pad_type
             in_chs = sd_chs
